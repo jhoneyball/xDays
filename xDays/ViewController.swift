f143fe46a54698801f619e6799f682b8591eaab0
James <james@honeyball.com>
2017-03-12 15:10:15 +0000

    initial commit

--- xDays/ViewController.swift
+++ xDays/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var xDaysDate = Date()
+    
+    @IBAction func updateDate(sender: UIDatePicker, forEvent event: UIEvent) {
+        
+            xDaysDate = sender.date
+            print ("The date is \(xDaysDate)")
+            updateTheDateBadge(daysUntil: xDaysDate)
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateTheDateBadge(daysUntil: xDaysDate)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
 
+func updateTheDateBadge(daysUntil toDate: Date)
+{
+    let application = UIApplication.shared
+    var badgeNumber = 0
+    
+    let settings = application.currentUserNotificationSettings
+    if (settings?.types.contains(.badge))! {
+        print ("Badges are true")
+    } else {
+        print ("Badges are not true")
+        application.registerUserNotificationSettings(UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil))
+        application.registerForRemoteNotifications()
+    }
+
+    let currentCalendar = Calendar.current
+
+    if let daysToGo = currentCalendar.dateComponents([.day],
+                                               from: Date(),
+                                               to: toDate).day {
+        if (daysToGo > 0 && daysToGo < 9999) {
+            badgeNumber = daysToGo
+        }
+    }
+    application.applicationIconBadgeNumber = badgeNumber
+}
